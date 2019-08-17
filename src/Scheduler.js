3a82c53a0e2fc52656754d1019fe69b01b508105
tesseradecades <nwe8978@rit.edu>
2019-08-17 16:37:55 -0400

    need to create a player component

--- src/Scheduler.js
+++ src/Scheduler.js
@@ -6,31 +6,63 @@ import "react-datepicker/dist/react-datepicker.css";
 class Scheduler extends React.Component{
     state={ 
         players : [],
-        startDate: new Date()
+        startDate: new Date(),
+        weeks:0
+    }
+
+    addPlayer = ()=>{
+        this.setState(state=>{
+            const list = state.players.push('');
+            return{list}
+        });
     }
 
     assignStart = (date) =>{
         this.setState({startDate:date});
-        console.log(this.state.startDate);
+    }
+
+    assignWeeks = (numWeeks)=>{
+        this.setState({weeks:numWeeks});
     }
 
     render(){
         return (
-            <div className="card text-center">
-                <div className="card-header">
-                    Welcome to the Belaheim Player Scheduler
+
+            <div className="card">
+                <div className="card-header text-center">
+                    <h5 className="card-title">Welcome to the Player Scheduler</h5>
                 </div>
                 <div className="card-body">
-                    <div className="input-group">
-                        <label for="startDatePicker">When does the campaign start?</label>
-                        <DatePicker 
-                            id="startDatePicker"
-                            selected={this.state.startDate}
-                            onChange={this.assignStart}
-                            />
+                    <label className="card-text" htmlFor="startDatePicker">When does the campaign begin?</label>
+                    <div className="input-group mx-auto mb-3">
+                            <DatePicker 
+                                id="startDatePicker"
+                                selected={this.state.startDate}
+                                onChange={this.assignStart}
+                                placeholderText="Start date"/>
                     </div>
+                    <label htmlFor="weeksInput">How long will the campaign last</label>
+                    <div className="input-group">
+                        <input 
+                            id="weeksInput"
+                            value={this.state.weeks}
+                            onChange={this.assignWeeks}/> weeks                       
+                    </div>
+                    <label>Players</label>
+                    <ol className="list-group">
+                        {
+                            this.state.players.map(player=>(
+                                    <li key={player}><input value={player}></input></li>
+                                )
+                            )
+                        }
+                    </ol>
+                    <button onClick={this.addPlayer}>Add Player</button>
                 </div>
             </div>
+
+
+            
         )
     }
 }
