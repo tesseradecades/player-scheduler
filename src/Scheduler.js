b4febc777eb4945f8dc74ac6e71957a5900103f3
tesseradecades <nwe8978@rit.edu>
2019-08-17 13:02:17 -0400

    removed unnecessary files and added datepicker

---
+++ src/Scheduler.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import DatePicker from "react-datepicker";
+ 
+import "react-datepicker/dist/react-datepicker.css";
+
+class Scheduler extends React.Component{
+    state={ 
+        players : [],
+        startDate: new Date()
+    }
+
+    assignStart = (date) =>{
+        this.setState({startDate:date});
+        console.log(this.state.startDate);
+    }
+
+    render(){
+        return (
+            <div className="card">
+                <div className="card-header">
+                    Welcome to the Belaheim Player Scheduler
+                </div>
+                <div className="card-body">
+                    <div className="input-group">
+                        <label for="startDatePicker">When does the campaign start?</label>
+                        <DatePicker 
+                            id="startDatePicker"
+                            selected={this.state.startDate}
+                            onChange={this.assignStart}
+                            />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Scheduler
