dbd5f19b8862b41253edb2f4bcb34b3906dbaf01
tesseradecades <nwe8978@rit.edu>
2019-08-17 20:31:51 -0400

    fixed bindings and made fields for all required info

--- src/Scheduler.js
+++ src/Scheduler.js
@@ -5,29 +5,37 @@ import "react-datepicker/dist/react-datepicker.css";
 
 class Scheduler extends React.Component{
     state={ 
-        players : [],
+        players : '',
         startDate: new Date(),
-        weeks:0
+        weeks:0,
+        offPlayers:0
     }
 
-    addPlayer = ()=>{
-        this.setState(state=>{
-            const list = state.players.push('');
-            return{list}
-        });
+    assignStart = (e) =>{
+        this.setState({startDate:e.target.value});
     }
 
-    assignStart = (date) =>{
-        this.setState({startDate:date});
+    assignWeeks = (e)=>{
+        this.setState({weeks:e.target.value});
     }
 
-    assignWeeks = (numWeeks)=>{
-        this.setState({weeks:numWeeks});
+    submitParams = () =>{
+        console.log(`Start ${this.state.startDate}`);
+        console.log(`Weeks ${this.state.weeks}`);
+        console.log(`Players ${this.state.players}`);
+        console.log(`Off Players ${this.state.offPlayers}`);
+    }
+
+    updateOffPlayers = (e) =>{
+        this.setState({offPlayers:e.target.value});
+    }
+
+    updatePlayers = (e) =>{
+        this.setState({players:e.target.value});
     }
 
     render(){
         return (
-
             <div className="card">
                 <div className="card-header text-center">
                     <h5 className="card-title">Welcome to the Player Scheduler</h5>
@@ -35,29 +43,36 @@ class Scheduler extends React.Component{
                 <div className="card-body">
                     <label className="card-text" htmlFor="startDatePicker">When does the campaign begin?</label>
                     <div className="input-group mx-auto mb-3">
-                            <DatePicker 
-                                id="startDatePicker"
-                                selected={this.state.startDate}
-                                onChange={this.assignStart}
-                                placeholderText="Start date"/>
+                        <DatePicker 
+                            id="startDatePicker"
+                            selected={this.state.startDate}
+                            onChange={this.assignStart}
+                            placeholderText="Start date"/>
                     </div>
                     <label htmlFor="weeksInput">How long will the campaign last</label>
                     <div className="input-group">
                         <input 
                             id="weeksInput"
                             value={this.state.weeks}
-                            onChange={this.assignWeeks}/> weeks                       
+                            onChange={this.assignWeeks}
+                            placeholder="number of weeks"/> weeks                       
                     </div>
-                    <label>Players</label>
-                    <ol className="list-group">
-                        {
-                            this.state.players.map(player=>(
-                                    <li key={player}><input value={player}></input></li>
-                                )
-                            )
-                        }
-                    </ol>
-                    <button onClick={this.addPlayer}>Add Player</button>
+                    <label htmlFor="players">Players</label>
+                    <div className="input-group">
+                        <input
+                            id="players"
+                            value={this.state.players}
+                            onChange={this.updatePlayers}
+                            placeholder="player names"/>
+                    </div>
+                    <label htmlFor="offPlayers">Number of players off in a week</label>
+                    <div className="input-group">
+                        <input
+                                id="offPlayers"
+                                value={this.state.offPlayers}
+                                onChange={this.updateOffPlayers}/>
+                    </div>
+                    <button onClick={this.submitParams}>Submit</button>
                 </div>
             </div>
 
