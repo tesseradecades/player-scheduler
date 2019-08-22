75ef62be039fb9e060010ab5de8d6898567600d2
tesseradecades <nwe8978@rit.edu>
2019-08-22 07:36:28 -0400

    updated client to reflect staging

---
+++ client/src/test/Scheduler.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Scheduler from '../Scheduler';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Scheduler />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
